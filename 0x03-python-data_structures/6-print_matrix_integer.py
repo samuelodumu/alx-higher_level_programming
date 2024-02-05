6465099c43dd138daa055d6362ad6994b5895b68
Samuel Odumu <themainsamuel@gmail.com>
2024-02-05 06:45:32 -0800

    fixed pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for idx, j in enumerate(i):
             if idx < len(i) - 1:
-                print('{:d}'.format(j), end= ' ')
+                print('{:d}'.format(j), end=' ')
             else:
-                print('{:d}'.format(j), end= '')
+                print('{:d}'.format(j), end='')
         print()
