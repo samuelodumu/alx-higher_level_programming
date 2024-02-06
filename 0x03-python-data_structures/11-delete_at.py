ce4e5ac518dc901eb8b2c874d727537b84d08c82
Samuel Odumu <themainsamuel@gmail.com>
2024-02-06 09:10:41 -0800

    added if `idx` is negative condition

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,8 @@
 def delete_at(my_list=[], idx=0):
     if idx >= len(my_list):
         return my_list
+    elif idx < 0:
+        return my_list
     else:
         del my_list[idx]
         return my_list
