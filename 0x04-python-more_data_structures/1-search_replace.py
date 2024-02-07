be4faa37a7f5a14ddd1f253601a6f26d3898e4e9
Samuel Odumu <themainsamuel@gmail.com>
2024-02-07 16:23:38 +0100

    actually understood the question

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,9 +4,12 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
 
-    if search > len(my_list):
+    if search not in my_list:
         return my_list
     else:
-        new_list.insert(search - 1, replace)
-        del new_list[search]
+        x = new_list.count(search)
+        for i in range(x):
+            idx = new_list.index(search)
+            new_list.remove(search)
+            new_list.insert(idx, replace)
         return new_list
