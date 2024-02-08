1b3531b8b4b9870f1fb0dab2bd278719818368c5
Samuel Odumu <themainsamuel@gmail.com>
2024-02-08 17:15:31 +0100

    fixed pycodestyle

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,11 +3,8 @@
 
 def print_sorted_dictionary(a_dictionary):
     tup_items = tuple(a_dictionary.items())
-    sorted_tup = sorted(tup_items, key=lambda x:x[0])
+    sorted_tup = sorted(tup_items, key=lambda x: x[0])
     sorted_dict = dict(sorted_tup)
-    
+
     for i in sorted_dict.items():
         print("{} : {}".format(i[0], i[1]))
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
