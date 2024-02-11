eed582810b85166d23382299e1f1e4aa4b3db1d7
Samuel Odumu <themainsamuel@gmail.com>
2024-02-11 20:22:33 +0100

    fixed format

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     while counter < x:
         try:
-            print(my_list[counter], end=' ')
+            print(my_list[counter], end='')
             counter += 1
             continue
         except IndexError:
