c8b7f0727b2e67013ea7c93e5f023cc680839291
Samuel Odumu <themainsamuel@gmail.com>
2024-02-11 20:20:42 +0100

    task 0: Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    while counter < x:
+        try:
+            print(my_list[counter], end=' ')
+            counter += 1
+            continue
+        except IndexError:
+            break
+    print()
+    return counter
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,7 @@
+## 0x05. Python - Exceptions
+
+My answers to the Python Exceptions project.
+
+PS: These answers are mine and were not copied from anywhere, I would appreciate it if I'm also not copied.
+
+I'm also open to collaboration (Peer learning)
