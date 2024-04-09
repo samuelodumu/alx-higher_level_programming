30f4ad7cb6e4170ce7978e110264a966b0cea0c2
Samuel <themainsamuel@gmail.com>
2024-04-09 10:06:15 +0100

    task 1: Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with a square class"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """Instantiation of objects"""
+        self.__size = size
