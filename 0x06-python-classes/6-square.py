32b6ae13bb7faf61560037ec29527ab80b1682b9
Samuel <themainsamuel@gmail.com>
2024-04-09 18:42:06 +0100

    fixed y-axis printing format

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,8 +49,10 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for y in range(self.__position[1]):
+                print()
             for i in range(self.__size):
-                for k in range(self.__position[0]):
+                for x in range(self.__position[0]):
                     print(" ", end='')
                 for j in range(self.__size):
                     print("#", end='')
