f38e1757ce1e05935321a63aaa817a3d4d0012dc
Samuel Odumu <themainsamuel@gmail.com>
2024-04-25 03:00:15 +0100

    task 3 completed tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""Defines 'say_my_name' function"""
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints the names of the person passed to the function
-    """
+    """Prints the names of the person passed to the function"""
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
