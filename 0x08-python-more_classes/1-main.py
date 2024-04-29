87aede22b8fde4a7ea8e3f05d30cca9dabaf1d83
Samuel Odumu <themainsamuel@gmail.com>
2024-04-30 00:19:42 +0100

    I'm loosing it

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -7,7 +7,3 @@ print(my_rectangle.__dict__)
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
-
-guillaume@ubuntu:~/0x08$ ./1-main.py
-{'_Rectangle__height': 4, '_Rectangle__width': 2}
-{'_Rectangle__height': 3, '_Rectangle__width': 10}
