52f435ac3f4b548036b2e3b6a8c38b0185356a95
Samuel Odumu <themainsamuel@gmail.com>
2024-05-09 22:48:37 +0100

    task 3 and 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -58,3 +58,6 @@ class Rectangle:
                 if i < self.__height - 1:
                     print()
             return ""
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
