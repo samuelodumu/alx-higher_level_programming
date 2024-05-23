ecbe446453709d379b47af408f7349ba5e8fb7c6
Samuel Odumu <themainsamuel@gmail.com>
2024-05-23 05:55:13 +0100

    removed properties

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,23 +5,23 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class"""
-    no_exception = True
 
     def __init__(self, width, height):
+        no_exception = True
         try:
             self.integer_validator("width", width)
         except Exception as e:
-            print(f"{e}")
-            self.no_exception = False
+            print(f"{e.__class__.__name__} {e}")
+            no_exception = False
 
-        if self.no_exception:
+        if no_exception:
             self.__width = width
 
         try:
             self.integer_validator("height", height)
         except Exception as e:
-            print(f"{e}")
-            self.no_exception = False
+            print(f"{e.__class__.__name__} {e}")
+            no_exception = False
 
-        if self.no_exception:
+        if no_exception:
             self.__height = height
