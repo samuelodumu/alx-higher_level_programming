611e2497199d806a28a92442195ab62d2b7c927c
Samuel Odumu <themainsamuel@gmail.com>
2024-05-23 02:46:53 +0100

    task 6: Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains the `BaseGeometry` class"""
+
+
+class BaseGeometry:
+    """Contains `area` function"""
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
