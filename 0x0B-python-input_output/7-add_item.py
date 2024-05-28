0b4507906c6d4a47658b53bad0cf0ff2fc472b5f
Samuel Odumu <themainsamuel@gmail.com>
2024-05-28 18:14:23 +0100

    task 7: Load, add, save (took me a while)

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Contains `main` function"""
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def main():
+    """adds all arguments to a Python list, and then saves them to a file"""
+    new_list = []
+    filename = "add_item.json"
+
+    for arg in sys.argv[1:]:
+        new_list.append(arg)
+    save_to_json_file(new_list, filename)
+
+    return load_from_json_file(filename)
+
+
+if __name__ == "__main__":
+    main()
