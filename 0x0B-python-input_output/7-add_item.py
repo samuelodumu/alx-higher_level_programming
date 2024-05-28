26739e66ba32041f3cb2cd37eb5cb82ba5fa2cec
Samuel Odumu <themainsamuel@gmail.com>
2024-05-28 21:37:40 +0100

    modified task 7 to append instead of simply write

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Contains `main` function"""
 import json
+import os
 import sys
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -11,11 +13,19 @@ def main():
     new_list = []
     filename = "add_item.json"
 
-    for arg in sys.argv[1:]:
-        new_list.append(arg)
-    save_to_json_file(new_list, filename)
+    if os.path.exists(filename):
+        new_list = load_from_json_file(filename)
 
-    return load_from_json_file(filename)
+        for arg in sys.argv[1:]:
+            new_list.append(arg)
+        save_to_json_file(new_list, filename)
+        return load_from_json_file(filename)
+
+    else:
+        for arg in sys.argv[1:]:
+            new_list.append(arg)
+        save_to_json_file(new_list, filename)
+        return load_from_json_file(filename)
 
 
 if __name__ == "__main__":
