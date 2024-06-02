ff59a732ab6b653be3870401605a88e2053289e7
Samuel Odumu <themainsamuel@gmail.com>
2024-06-02 02:14:06 +0100

    task 12: Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing the
+    Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+    else:
+        pascal_triangle = []
+
+        for i in range(n):
+            row = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(pascal_triangle[i - 1][j - 1]
+                               + pascal_triangle[i - 1][j])
+            pascal_triangle.append(row)
+
+        return pascal_triangle
