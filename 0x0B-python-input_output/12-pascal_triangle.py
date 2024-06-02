6eaf92f8f7aabda9fe5663198caa3e6ab6e0e014
Samuel Odumu <themainsamuel@gmail.com>
2024-06-02 02:16:14 +0100

    fixed pycodestyle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Contains `pascal_triangle` function"""
 
+
 def pascal_triangle(n):
     """returns a list of lists of integers representing the
     Pascal’s triangle of n"""
