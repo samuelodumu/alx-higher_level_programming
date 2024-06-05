656259e6349815a5eab8284a3ba52e6059f8d769
Samuel Odumu <themainsamuel@gmail.com>
2024-06-06 00:55:08 +0100

    task 15: Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Contains `base` class"""
+import json
 
 
 class Base:
@@ -13,3 +14,22 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of `list_dictionaries`"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return f"[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of `list_objs` to a file"""
+        for obj in list_objs:
+            to_json_string(obj)
+        with open(f"{cls}.json", "w", encoding="utf-8"):
+            if list_objs is None:
+                f"{cls}.json".write("[]")
+            else:
+                json.dump(list_objs, f"{cls}.json")
