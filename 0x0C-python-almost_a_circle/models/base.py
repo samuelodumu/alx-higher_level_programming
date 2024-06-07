1f8fcd912386bbb5efcfa49aeea0cd70ce8ef28c
Samuel Odumu <themainsamuel@gmail.com>
2024-06-07 02:24:04 +0100

    task 17 and 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,18 @@ class Base:
                         list_dicts.append(obj_dict_rep)
                 serialized_str = cls.to_json_string(list_dicts)
                 f.write(serialized_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        demo = cls(8, 4)
+        demo.update(**dictionary)
+        return demo
