7913fbf27bf85e4dbaf21669bb47cc537d31fa98
Samuel <themainsamuel@gmail.com>
2024-06-20 05:59:00 +0100

    task 11: Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,25 @@ const process = require('node:process');
 
 const args = process.argv.slice(2);
 
-function factorial (a) {
-  if (isNaN(a) || a === 0) {
-    return 1;
-  } else {
-    return (a * factorial(a - 1));
-  }
-}
+const numbers = [];
 
-console.log(factorial(args[0]));
+if (args.length === 0 || args.length === 1) {
+  console.log(0);
+} else {
+  for (const arg of args) {
+    numbers.push(arg);
+  }
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (const number of numbers) {
+    if (number > largest) {
+      secondLargest = largest;
+      largest = number;
+    } else if (number > secondLargest && number !== largest) {
+      secondLargest = number;
+    }
+  }
+
+  console.log('Second largest number:', secondLargest);
+}
