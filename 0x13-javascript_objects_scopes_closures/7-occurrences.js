a9fe94282d137577635d1b69dc0c278a334e7c72
Samuel <themainsamuel@gmail.com>
2024-06-21 06:19:02 +0100

    task 7: apparently you should put '{}' around your export functions

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,12 +2,12 @@
 
 function nbOccurences (list, searchElement) {
   let nbo = 0;
-  for (const x in list) {
-    if (x === searchElement) {
+  for (const elem of list) {
+    if (elem === searchElement) {
       nbo++;
     }
   }
   return nbo;
 }
 
-module.exports = nbOccurences;
+module.exports = { nbOccurences };
