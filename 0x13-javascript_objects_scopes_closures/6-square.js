ea308f45504b33d95af425d813225f047a8527cc
Samuel <themainsamuel@gmail.com>
2024-06-21 05:39:32 +0100

    task 6: fixed inheritance issues

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const BaseSquare = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends BaseSquare {
   charPrint (c) {
     for (let i = 0; i < this.height; i++) {
       let output = '';
