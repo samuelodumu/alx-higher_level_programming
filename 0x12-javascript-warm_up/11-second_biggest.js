96a4a8179af6e76eeddd71d6d2c2a7b448ab2f8d
Samuel <themainsamuel@gmail.com>
2024-06-22 00:29:30 +0100

    modified task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,23 +6,25 @@ const args = process.argv.slice(2);
 
 const numbers = [];
 
-if (args.length === 0 || args.length === 1) {
+if (args.length < 2) {
   console.log(0);
-} else {
-  for (const arg of args) {
-    numbers.push(arg);
-  }
-  let largest = -Infinity;
-  let secondLargest = -Infinity;
-
-  for (const number of numbers) {
-    if (number > largest) {
-      secondLargest = largest;
-      largest = number;
-    } else if (number > secondLargest && number !== largest) {
-      secondLargest = number;
-    }
-  }
-
-  console.log('Second largest number:', secondLargest);
+  process.exit();
 }
+
+for (const arg of args) {
+  numbers.push(arg);
+}
+
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (const number of numbers) {
+  if (parseInt(number) > largest) {
+    secondLargest = largest;
+    largest = parseInt(number);
+  } else if (parseInt(number) < largest && parseInt(number) > secondLargest) {
+    secondLargest = parseInt(number);
+  }
+}
+
+console.log(secondLargest);
