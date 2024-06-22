45adc62e31f9576c9bcd4ef6cf6d0084166e670b
Samuel <themainsamuel@gmail.com>
2024-06-22 05:51:38 +0100

    task 17: Increment object

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function (value) {
+  value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
