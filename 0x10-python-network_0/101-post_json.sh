5d9a75fe9ca9c111c9469df6c8b3595455833b75
Samuel <themainsamuel@gmail.com>
2024-07-12 16:33:44 +0100

    bash syntax is a piece of work

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a json post request to a url passed as the first argument, and displays only the status code of the response
-curl -s -X POST -H "Content-Type: application/json" -d `cat $2` $1
+curl -s -X POST -H "Content-Type: application/json" -d "$(cat $2)" $1
