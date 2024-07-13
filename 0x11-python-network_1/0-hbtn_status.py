877ec65a175b51e7768679a12cae9c471427127a
Samuel <themainsamuel@gmail.com>
2024-07-13 16:43:46 +0100

    modified task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status using urllib"""
-import urllib.request as url
 
-req = url.Request('https://alx-intranet.hbtn.io/status')
-with url.urlopen(req) as file:
-    page_content = file.read()
-
-print(page_content)
+if __name__ == '__main__':
+    import urllib.request as url
+    req = url.Request('https://alx-intranet.hbtn.io/status')
+    with url.urlopen(req) as response:
+        data = response.read()
+        utf_data = data.decode()
+        content_type = type(data)
+        print(f'Body response:\n\t- type: {content_type}\n\t- content: {data}'
+              f'\n\t- utf8 content: {utf_data}')
