c87802d048bd1bd9739e0fa80cd2ae5b1e0befa3
Samuel <themainsamuel@gmail.com>
2024-07-14 17:05:09 +0100

    task 5: Response header value #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays
-the body of the response (decoded in utf-8)"""
+"""fetches https://alx-intranet.hbtn.io/status using requests"""
 
 import requests
 
---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""sends a request to a given URL and displays the value
+of the variable X-Request-Id in the response header"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    req = requests.get(sys.argv[1])
+    print(f"{req.headers['X-Request-Id']}")
