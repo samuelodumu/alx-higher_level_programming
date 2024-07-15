f710b033607df03399cde63aff5066453260f5ea
Samuel <themainsamuel@gmail.com>
2024-07-15 14:25:38 +0100

    task 9: done?

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """uses the GitHub API to display a given users id"""
 
-import requests as r
+import requests
 import sys
 
 if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
-    url = f'https://api.github.com/users/{username}'
+    url = f'https://api.github.com/user'
+    header = {"Authorization": f"Bearer {password}"}
 
-    res = r.get(url, auth=r.auth.HTTPBasicAuth(username, password))
-    if res.status_code == 200:
-        print(res.json()['id'])
+    res = requests.get(url, headers=header)
+    json_res = res.json()
+
+    if res.status_code >= 400:
+        print('None')
     else:
-        print(None)
+        print(json_res.get('id'))
