cd372984b6ec79a79483bc51713ec2273b70852f
Samuel <themainsamuel@gmail.com>
2024-07-31 15:09:51 +0100

    fixed task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,11 +4,10 @@ const args = process.argv.slice(2);
 const request = require('request');
 
 const endpoint = args[0];
-request.get(endpoint, (error, response, body) => {
+request(endpoint, (error, response, body) => {
   if (error) {
     console.error(error);
   }
-  console.log(body)
-  const count = body.split('people/18').length - 1
+  const count = body.split('people/18').length - 1;
   console.log(count);
 });
